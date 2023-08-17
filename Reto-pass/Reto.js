1f50bd2cc250c9a10d91523e428021b7972d6446
YaelCano <78746517+YaelCano@users.noreply.github.com>
2023-08-17 07:21:35 -0500

    Update Reto.js

--- Reto-pass/Reto.js
+++ Reto-pass/Reto.js
@@ -1,32 +1,47 @@
-/**################################*/
-/*RETO PROGRAMACIÓN - PASSWORD CREATION*/
-
 function newPassword(a, b) {
-    let result = ''; // La cadena combinada
-    
-    let i = 0; // Índice para la cadena a
-    let j = 0; // Índice para la cadena b
-    
-    while (i < a.length && j < b.length) {
-        result += a[i] + b[j];
-        i++;
-        j++;
-    }
-    
-    // Agregar los caracteres restantes de la cadena a
-    while (i < a.length) {
-        result += a[i];
-        i++;
-    }
-    
-    // Agregar los caracteres restantes de la cadena b
-    while (j < b.length) {
-        result += b[j];
-        j++;
-    
-    }
-    
-    }
-    console.log(newPassword("antonio","maria"));
-    /*Desarrollado por: Yael Mauricio Cano Palacio*/
-    
+  let result = '';
+
+  let i = 0;
+  let j = 0;
+
+  while (i < a.length && j < b.length) {
+    result += a[i] + b[j];
+    i++;
+    j++;
+  }
+
+  while (i < a.length) {
+    result += a[i];
+    i++;
+  }
+
+  while (j < b.length) {
+    result += b[j];
+    j++;
+  }
+
+  return result;
+}
+
+// Casos de prueba
+const testCases = [
+  { a: 'abc', b: 'deft', expected: 'adbect' },
+  { a: 'hackerrank', b: 'mountain', expected: 'hmaocuknetrariannk' },
+  // Agrega más casos de prueba aquí
+];
+
+// Función para ejecutar las pruebas
+function runTests() {
+  for (const testCase of testCases) {
+    const result = newPassword(testCase.a, testCase.b);
+    const isPassing = result === testCase.expected;
+    console.log(
+      `Caso de prueba: a = '${testCase.a}', b = '${testCase.b}' - Resultado: '${result}', Esperado: '${testCase.expected}' - ${
+        isPassing ? 'PASSED' : 'FAILED'
+      }`
+    );
+  }
+}
+
+// Ejecutar las pruebas
+runTests();
